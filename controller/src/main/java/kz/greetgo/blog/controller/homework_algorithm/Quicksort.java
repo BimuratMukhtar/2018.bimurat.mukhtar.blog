3de6666d2f5d10334569e0c4356f092bdeb2c4f9
Mukhtar Bimurat <Mukhtar Bimurat>
2018-03-16 15:31:08 +0600

    added error for quicksort

--- controller/src/main/java/kz/greetgo/blog/controller/homework_algorithm/Quicksort.java
+++ controller/src/main/java/kz/greetgo/blog/controller/homework_algorithm/Quicksort.java
@@ -29,9 +29,7 @@ public class Quicksort {
             }
             // If the current value from the right list is larger than the pivot
             // element then get the next element from the right list
-            while (numbers[j] > pivot) {
-                j--;
-            }
+
 
             // If we have found a value in the left list which is larger than
             // the pivot element and if we have found a value in the right list
@@ -47,8 +45,7 @@ public class Quicksort {
         // Recursion
         if (low < j)
             quicksort(low, j);
-        if (i < high)
-            quicksort(i, high);
+
     }
 
     private void exchange(int i, int j) {
